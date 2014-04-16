0213d089beac114d8328b26db765ccf1a1a927fb
Pavel S. Khmelinsky <hmepas@gmail.com>
2014-04-16 12:37:57 +0400

    + linux-deploy.sh vundle install

--- linux-deploy.sh
+++ linux-deploy.sh
@@ -3,3 +3,9 @@
 cp -r dot.vim ~/.vim
 cp dot.vimrc ~/.vimrc
 cp dot.vimrc-lang-remap ~/.vimrc-lang-remap
+
+echo "installin vundle"
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+echo "installing vundle plugins"
+vim +PluginInstall +qall

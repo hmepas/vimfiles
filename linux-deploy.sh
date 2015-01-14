bf186ce5fb86b659408739a0f4d3e392e68db41e
Pavel S. Khmelinsky <hmepas@gmail.com>
2015-01-14 17:17:44 +0300

    + perl_incl script for composing perl include paths file related for syntastic

--- linux-deploy.sh
+++ linux-deploy.sh
@@ -3,6 +3,7 @@
 cp -r dot.vim ~/.vim
 cp dot.vimrc ~/.vimrc
 cp dot.vimrc-lang-remap ~/.vimrc-lang-remap
+cp perl_incl ~/bin/
 
 echo "installin vundle"
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
